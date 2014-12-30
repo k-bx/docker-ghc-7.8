ddb57ba6e87db8f7ec355499a43eecac667b8ec0
Konstantine Rybnikov <k-bx@k-bx.com>
2014-12-30 18:39:16 +0200

    Add LC_ALL to .profile

--- docker_common_start.sh
+++ docker_common_start.sh
@@ -1,7 +1,7 @@
 apt-get update
 locale-gen en_US.UTF-8
 export LC_ALL='en_US.UTF-8'
-LC_ALL en_US.UTF-8
+echo "export LC_ALL=en_US.UTF-8" >> /root/.profile
 echo 'root:docker' |chpasswd
 mkdir -p ${HOME}/.ssh/
 apt-get install -y openssh-server
