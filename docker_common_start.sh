aa38871375b6ecda267a5fc11df6ef848c2b2395
Konstantine Rybnikov <k-bx@k-bx.com>
2014-12-30 18:40:48 +0200

    install apt-utils

--- docker_common_start.sh
+++ docker_common_start.sh
@@ -4,5 +4,6 @@ export LC_ALL='en_US.UTF-8'
 echo "export LC_ALL=en_US.UTF-8" >> /root/.profile
 echo 'root:docker' |chpasswd
 mkdir -p ${HOME}/.ssh/
+apt-get install -y apt-utils
 apt-get install -y openssh-server
 mkdir /var/run/sshd
