ac18ad29b1c3125891d21ec75e0b2f3231859be1
Konstantine Rybnikov <k-bx@k-bx.com>
2015-04-09 12:44:44 +0300

    Install yesod on stackaged

--- scripts/install_stackage_packages.sh
+++ scripts/install_stackage_packages.sh
@@ -4,4 +4,4 @@ cd /tmp/tmp_stackage_repo
 curl http://www.stackage.org/lts/cabal.config -o cabal.config
 cabal update
 # some random set of packages we want to have bootstrapped
-cabal install wreq scotty snap
+cabal install yesod scotty snap pipes
