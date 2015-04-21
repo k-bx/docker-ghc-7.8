7092b0fd454f22be024bff3988d3c755de195ff5
Konstantine Rybnikov <k-bx@k-bx.com>
2015-04-21 20:14:19 +0300

    Add old-time

--- scripts/install_stackage_packages.sh
+++ scripts/install_stackage_packages.sh
@@ -5,6 +5,5 @@ stackage update
 mkdir -p /tmp/tmp_stackage_repo
 cd /tmp/tmp_stackage_repo
 stackage sandbox init
-stackage init
 # some random set of packages we want to have bootstrapped
-cabal install yesod scotty snap pipes
+cabal install yesod scotty snap pipes ekg hedis haskell-src-exts QuickCheck old-time
