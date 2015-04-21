04272071db9aefe8323ecdbabf619fbad19628e4
Konstantine Rybnikov <k-bx@k-bx.com>
2015-04-21 18:52:13 +0300

    Use stackage tools

--- scripts/install_stackage_packages.sh
+++ scripts/install_stackage_packages.sh
@@ -1,7 +1,10 @@
 . ~/.profile
+cabal update
+cabal install stackage-update stackage-cli
+stackage update
 mkdir -p /tmp/tmp_stackage_repo
 cd /tmp/tmp_stackage_repo
-curl http://www.stackage.org/lts/cabal.config -o cabal.config
-cabal update
+stackage sandbox init
+stackage init
 # some random set of packages we want to have bootstrapped
 cabal install yesod scotty snap pipes
